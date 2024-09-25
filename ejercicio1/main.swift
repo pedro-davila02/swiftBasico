59ec8c0f499c0a1b6b77fcf16247c818f75e4d42
Pedro Alumno <pedro_maria_davila_m-tsapp1ma2324@cev.com>
2024-09-25 09:26:12 +0100

    Initial Commit

---
+++ ejercicio1/main.swift
@@ -0,0 +1,10 @@
+import Foundation
+var suma: Double = 0
+var contador = 0
+
+while contador < 5 {
+    print("Introduce el número \(contador + 1): ", terminator: "")
+    zsqwxdwxdxwxdwxdsz
+}
+
+
